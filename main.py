137c017bcaa9f8e95075d82cb077cdbe5ae77d50
rmalesich <ryan-malesich@uiowa.edu>
2023-09-24 12:17:56 -0500

    Code Polished

--- main.py
+++ main.py
@@ -5,51 +5,45 @@ import requests
 import pandas as pd
 from taipy.gui import Gui, notify
 
-API_KEY = "ADD YOUR OPENAI API KEY HERE"
+API_KEY = "YOUR_API_KEY"
 INITIAL_PROMPT = "N/A"
 MAX_TOKENS = 150
+
 Filament_Type = ''
 answer = ''
 result = ''
 
-TotalCost = 0
-
+user_message = ''
+user_message2 = ''
 
 API_ENDPOINT = "https://api.openai.com/v1/chat/completions"
-def generate_completion(messages, model="gpt-3.5", temperature=1):
+
+
+def generate_completion(messages, model="gpt-3.5-turbo-0613", temperature=1):
     headers = {
         "Content-Type": "application/json",
         "Authorization": f"Bearer {API_KEY}",
     }
 
-    data = {
-        "model": model,
-        "messages": messages,
-        "temperature": temperature,
-    }
-
-    data["max_tokens"] = MAX_TOKENS
+    data = {"model": model, "messages": messages, "temperature": temperature, "max_tokens": MAX_TOKENS}
 
     response = requests.post(API_ENDPOINT, headers=headers, data=json.dumps(data))
 
     if response.status_code == 200:
         return response.json()["choices"][0]["message"]["content"]
     else:
-        return "I'm sorry, GPT-4 is not available right now."
+        return "I'm sorry, GPT-3.5 is not available right now."
 
 
 saved_messages = [
     {"role": "system", "content": INITIAL_PROMPT},
 ]
 
-user_message = ""
-user_message2 = ""
+
 def messages_to_data(messages):
     result = []
     for message in messages:
-        result_message = {}
-        result_message["Role"] = message["role"]
-        result_message["Message"] = message["content"]
+        result_message = {"Role": message["role"], "Message": message["content"]}
         if result_message["Role"] == "system":
             result_message["Role"] = "Result: "
         else:
@@ -68,13 +62,38 @@ def on_send_click(state):
     response = generate_completion(state.saved_messages)
     state.saved_messages.append({"role": "system", "content": response})
     state.saved_messages = state.saved_messages
-    notify(state, "success", "GPT-4 generated a response!")
+    notify(state, "success", "GPT-3.5 generated a response!")
 
 
 def get_data(path_to_csv: str):
     dataset = pd.read_csv(path_to_csv)
     dataset["Date"] = pd.to_datetime(dataset["Date"])
     return dataset
+
+
+def change_toPLAaction(state):
+    state.Filament_Type = "PLA"
+    notify(state, 'info', f'The Filament type is: {state.Filament_Type}')
+
+
+def change_toPETGaction(state):
+    state.Filament_Type = "PETG"
+    notify(state, 'info', f'The Filament type is: {state.Filament_Type}')
+
+
+def change_toABSaction(state):
+    state.Filament_Type = "ABS"
+    notify(state, 'info', f'The Filament type is: {state.Filament_Type}')
+
+
+def button_Action(state):
+    notify(state, "info", "Generating response...")
+    time.sleep(3)
+    answer = state.user_message2
+    result = answer
+    TotalCost = (random.randint(9, 17) * int(result)) / 1000
+    notify(state, "Success", f"Cost: ${TotalCost}", duration=7000)
+
 
 path_to_csv = "out.csv"
 dataset = get_data(path_to_csv)
@@ -89,7 +108,6 @@ page = """
 <|PETG|button|on_action=change_toPETGaction|>
 <|ABS|button|on_action=change_toABSaction|>
 
-
 <|{user_message2}|input|multiline=false|lines_shown=2|label=Input Grams|on_action=on_send_click|class_name=smaller-input-box|>
 
 <|Enter|button|on_action=button_Action|>
@@ -97,7 +115,9 @@ page = """
 <br/>
 <br/>
 <br/>
-### <p style="text-align: center;">What can I help you with?</p>
+
+### <p style="text-align: center;">What Can I Help You With?</p>
+
 <|{messages_to_data(saved_messages)}|table|show_all|width=100%|>
 
 <|{user_message}|input|multiline=True|lines_shown=2|label=Your Message|on_action=on_send_click|class_name=fullwidth|>
@@ -105,32 +125,10 @@ page = """
 <|Send|button|on_action=on_send_click|>
 
 ### <p style="text-align: center;">Average Cost of Filament</p>
+
 <|{dataset[1000:]}|chart|type=bar|x=Date|y=Cost|>
 
 <|{datasetTable}|table|width=100%|>
 """
-def change_toPLAaction(state):
-    notify(state, 'info', f'The filiament type is: {state.Filament_Type}')
-    state.Filament_Type = "PLA"
-    Filament_Type = "PLA"
-
-def change_toPETGaction(state):
-    notify(state, 'info', f'The filiament type is: {state.Filament_Type}')
-    state.Filament_Type = "PETG"
-    Filament_Type = "PETG"
-
-def change_toABSaction(state):
-    notify(state, 'info', f'The filiament type is: {state.Filament_Type}')
-    state.Filament_Type = "ABS"
-    Filament_Type = "ABS"
-
-def button_Action(state):
-    notify(state, "info", "Generating response...")
-    time.sleep(3)
-    answer = state.user_message2
-    result = answer
-    TotalCost = (random.randint(9, 17) * int(result)) / 1000
-    notify(state, "Success", f"Cost: ${TotalCost}", system_notification = True, duration = 7000)
-
 
 Gui(page).run()
